ea131a7ed0bb301aa5890ea5bce6e7008581aba3
nija123098 <nija123098@gmail.com>
2016-12-13 23:31:28 -0600

    Removes hardcoded emoticons

--- src/main/java/um/nija123098/inquisitor/util/MessageHelper.java
+++ src/main/java/um/nija123098/inquisitor/util/MessageHelper.java
@@ -3,12 +3,10 @@ package um.nija123098.inquisitor.util;
 import sx.blah.discord.handle.obj.IChannel;
 import sx.blah.discord.handle.obj.IMessage;
 import sx.blah.discord.util.MessageBuilder;
+import um.nija123098.inquisitor.bot.Entity;
 import um.nija123098.inquisitor.bot.Inquisitor;
 import um.nija123098.inquisitor.context.Channel;
 import um.nija123098.inquisitor.context.User;
-
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * Made by nija123098 on 11/6/2016
@@ -59,61 +57,11 @@ public class MessageHelper {
             return false;
         }
     }
-    private static final Map<String, String> MAP = new HashMap<String, String>();
-    public static String getEmoticon(String key){
-        return MAP.get(key);
-    }
+    private static final Entity EMOTICON_ENTITY;
     static {
-        MAP.put("floppy_disk", "\uD83D\uDCBE");
-        MAP.put("nerd", "\ud83e\udd13");
-        MAP.put("lock", "\uD83D\uDD12");
-        MAP.put("lock_and_key", "\uD83D\uDD10");
-        MAP.put("x", "\u274C");
-        MAP.put("question", "\u2753");
-        MAP.put("ok_hand", "\ud83d\udc4c");
-        MAP.put("tada", "\ud83D\uDC4C");
-        MAP.put("ping", "\ud83c\udfd3");
-        MAP.put("no_entry", "\u26D4");
-        MAP.put("no_entry_sign", "\ud83D\udEAB");
-        MAP.put("spy", "\uD83D\uDD75");
-        MAP.put("eye", "\ud83d\udc41");
-        MAP.put("thumbs_up", "\uD83D\uDC4D");
-        MAP.put("thumbs_down", "\uD83D\uDC4E");
-        MAP.put("0", "\u0030\u20E3");
-        MAP.put("1", "\u0031\u20E3");
-        MAP.put("2", "\u0032\u20E3");
-        MAP.put("3", "\u0033\u20E3");
-        MAP.put("4", "\u0034\u20E3");
-        MAP.put("5", "\u0035\u20E3");
-        MAP.put("6", "\u0036\u20E3");
-        MAP.put("7", "\u0037\u20E3");
-        MAP.put("8", "\u0038\u20E3");
-        MAP.put("9", "\u0039\u20E3");
-        MAP.put("a", "\ud83c\udde6");
-        MAP.put("b", "\ud83c\udde7");
-        MAP.put("c", "\ud83c\udde8");
-        MAP.put("d", "\ud83c\udde9");
-        MAP.put("e", "\ud83c\uddea");
-        MAP.put("f", "\ud83c\uddeb");
-        MAP.put("g", "\ud83c\uddec");
-        MAP.put("h", "\ud83c\udded");
-        MAP.put("i", "\ud83c\uddee");
-        MAP.put("j", "\ud83c\uddef");
-        MAP.put("k", "\ud83c\uddf0");
-        MAP.put("l", "\ud83c\uddf1");
-        MAP.put("m", "\ud83c\uddf2");
-        MAP.put("n", "\ud83c\uddf3");
-        MAP.put("o", "\ud83c\uddf4");
-        MAP.put("p", "\ud83c\uddf5");
-        MAP.put("q", "\ud83c\uddf6");
-        MAP.put("r", "\ud83c\uddf7");
-        MAP.put("s", "\ud83c\uddf8");
-        MAP.put("t", "\ud83c\uddf9");
-        MAP.put("u", "\ud83c\uddfa");
-        MAP.put("v", "\ud83c\uddfb");
-        MAP.put("w", "\ud83c\uddfc");
-        MAP.put("x", "\ud83c\uddfd");
-        MAP.put("y", "\ud83c\uddfe");
-        MAP.put("z", "\ud83c\uddff");
+        EMOTICON_ENTITY = Inquisitor.getEntity("emoticons");
+    }
+    public static String getEmoticon(String key){
+        return EMOTICON_ENTITY.getData(key, "unknown");
     }
 }
