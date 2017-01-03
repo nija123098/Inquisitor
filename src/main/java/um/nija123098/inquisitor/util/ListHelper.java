9bc4ff1a986203282f6b6525a29e346af47d86e0
Maik Wezinkhof <maik_wezinkhof@hotmail.com>
2017-01-03 10:36:09 +0100

    Might as well use E

--- src/main/java/um/nija123098/inquisitor/util/ListHelper.java
+++ src/main/java/um/nija123098/inquisitor/util/ListHelper.java
@@ -7,13 +7,13 @@ import java.util.List;
  * Made by nija123098 on 11/10/2016
  */
 public class ListHelper {
-    public static <E> void flip(List<E> list){
-        List<Object> objects = new ArrayList<Object>(list.size());
+    public static <E> void flip(List<E> list) {
+        List<E> objects = new ArrayList<>(list.size());
         for (int i = list.size() - 1; i >= 0; --i) {
             objects.add(list.get(i));
         }
         for (int i = 0; i < list.size(); i++) {
-            list.set(i, (E) objects.get(i));
+            list.set(i, objects.get(i));
         }
     }
 }
