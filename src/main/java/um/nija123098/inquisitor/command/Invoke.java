8eae520278fcf56e7ab5a73be52a3a1e92343c09
nija123098 <nija123098@gmail.com>
2017-01-13 21:45:43 -0600

    Adds return to Invoke#invoke methods

--- src/main/java/um/nija123098/inquisitor/command/Invoke.java
+++ src/main/java/um/nija123098/inquisitor/command/Invoke.java
@@ -11,14 +11,14 @@ import um.nija123098.inquisitor.util.StringHelper;
  * Made by nija123098 on 11/7/2016
  */
 public class Invoke {
-    public static void invoke(String user, String guild, String channel, String msg, IMessage iMessage){
+    public static boolean invoke(String user, String guild, String channel, String msg, IMessage iMessage){
         if (guild == null){
-            invoke(User.getUserFromID(user), null, Channel.getChannel(channel), msg, iMessage);
+            return invoke(User.getUserFromID(user), null, Channel.getChannel(channel), msg, iMessage);
         }else{
-            invoke(User.getUserFromID(user), Guild.getGuild(guild), Channel.getChannel(channel), msg, iMessage);
+            return invoke(User.getUserFromID(user), Guild.getGuild(guild), Channel.getChannel(channel), msg, iMessage);
         }
     }
-    public static void invoke(User user, Guild guild, Channel channel, String msg, IMessage iMessage){
+    public static boolean invoke(User user, Guild guild, Channel channel, String msg, IMessage iMessage){
         msg = StringHelper.limitOneSpace(msg);
         Command method = Registry.getCommand(msg);
         if (method != null){
@@ -26,9 +26,13 @@ public class Invoke {
             if (msg.length() > 0){
                 msg = msg.substring(1);
             }
-            method.invoke(user, guild, channel, msg, iMessage);
+            return method.invoke(user, guild, channel, msg, iMessage);
         }else{
             MessageHelper.react("question", iMessage);
+            if (guild == null){
+                MessageHelper.send(channel, "There is no need to have a prefix in a DM.");
+            }
         }
+        return false;
     }
 }
